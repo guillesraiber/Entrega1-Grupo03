e17e52908dffd4464b990ee82e9e4c91eeffe5ef
Martiniano Gutierrez <martu.gutierrez82@gmail.com>
2025-10-03 18:04:10 -0300

    Base pagina de juego. Cambio hover cards.

--- TP2/js/sidebar.js
+++ TP2/js/sidebar.js
@@ -1,10 +1,11 @@
-  const menuButton = document.querySelector('.menu-button');
+  const menuButton = document.querySelector('#nav-menu-img');
   const sidebar = document.querySelector('.sidebar');
   const mainContent = document.querySelector('.main-content');
+
   
   menuButton.addEventListener('click', function() {
       sidebar.classList.toggle('collapsed');
       mainContent.classList.toggle('sidebar-collapsed');
-      document.getElementById('menu-icon').src = sidebar.classList.contains('collapsed') ? 'images/nav_menu_open.svg' : 'images/nav_menu_collapse.svg';
+      menuButton.src = sidebar.classList.contains('collapsed') ? 'images/nav_menu_open.svg' : 'images/nav_menu_collapse.svg';
                                                                   // si el sidebar está colapsado, mostrar icono de abrir, sino de cerrar
   });
