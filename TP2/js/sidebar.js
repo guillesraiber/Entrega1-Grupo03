2dcf4e8c6bfc1376f7754a8b6d918cefc866f4f4
Martiniano Gutierrez <martu.gutierrez82@gmail.com>
2025-10-13 21:04:10 -0300

    Arreglo boton de abrir y cerrar sidebar en modo responsive

--- TP2/js/sidebar.js
+++ TP2/js/sidebar.js
@@ -1,7 +1,25 @@
   const menuButton = document.querySelector('#nav-menu-img');
   const sidebar = document.querySelector('.sidebar');
   const mainContent = document.querySelector('.main-content');
+  const logoNavbar = document.querySelector('.logo-full');
 
+      if (window.innerWidth <= 768) {
+        logoNavbar.src = 'images/logo/Logo-circular.png';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_collapse.svg' : 'images/buttons/nav_menu_open.svg';
+      } else {
+        logoNavbar.src = 'images/logo/Logo_horizontal.png';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_open.svg' : 'images/buttons/nav_menu_collapse.svg'; 
+      }
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth <= 768) {
+        logoNavbar.src = 'images/logo/Logo-circular.png';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_collapse.svg' : 'images/buttons/nav_menu_open.svg';
+      } else {
+        logoNavbar.src = 'images/logo/Logo_horizontal.png';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_open.svg' : 'images/buttons/nav_menu_collapse.svg'; 
+      }
+    })
   
   menuButton.addEventListener('click', function() {
     
@@ -10,10 +28,10 @@
       
       
       if (window.innerWidth >= 768) {
-        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_open.svg' : 'images/buttons/nav_menu_collapse.svg';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_collapse.svg' : 'images/buttons/nav_menu_open.svg';
         // si el sidebar está colapsado, mostrar icono de abrir, sino de cerrar
       } else { 
-        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_collapse.svg' : 'images/buttons/nav_menu_open.svg';
+        menuButton.src = sidebar.classList.contains('collapsed') ? 'images/buttons/nav_menu_open.svg' : 'images/buttons/nav_menu_collapse.svg'; 
         // espacio en blanco para que pongas el código necesario cuando la pantalla sea menor a 768px
       }
 });
