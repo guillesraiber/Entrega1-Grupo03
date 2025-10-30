0e8a965f51528f9f8d8dbb1676e9550d006e6437
Martiniano Gutierrez <martu.gutierrez82@gmail.com>
2025-10-29 21:29:53 -0300

    Hago pantalla de inicio funcional

--- TP4/js/peg/main.js
+++ TP4/js/peg/main.js
@@ -1,7 +1,24 @@
 import { GameController } from "./GameController.js";
 
-const game = new GameController();
-
-document.querySelector(".btn-restart").addEventListener("click", () => {
-    game.restart();
+document.querySelector(".instructions").addEventListener("click", () => {
+    
+    // oculto instrucciones
+    document.querySelector(".instructions").style.display = "none";
+    // muestro elementos del juego
+    mostrarElementosDelJuego();
+    
+    const game = new GameController();
+    document.querySelector(".btn-restart").addEventListener("click", () => {
+        game.restart();
+    });
 });
+
+// le saco el atributo hidden a los elementos del juego
+function mostrarElementosDelJuego() {
+    const elementosHidden = document.querySelectorAll('.hidden');
+
+    elementosHidden.forEach(elem => {
+    elem.classList.remove('hidden');
+    })
+
+}
