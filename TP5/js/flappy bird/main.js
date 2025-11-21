ee9244dd215694dee23272d36b4e281e4ba612e0
Martiniano Gutierrez <martu.gutierrez82@gmail.com>
2025-11-21 12:37:39 -0300

    Si se golpea al pez del menu 3 veces, explota. Los primeros golpes hacen animacion de golpe

--- TP5/js/flappy bird/main.js
+++ TP5/js/flappy bird/main.js
@@ -5,19 +5,38 @@ import { GameController } from "./GameController.js";
 
 // Si se aprieta al pescado en el manu, explota
 const playerElem = document.querySelector('#flappyPlayer');
-
+let hitCount = 0;
 playerElem.addEventListener('click', () => {
+    if (hitCount < 3) {
+        hitPlayer();
+    } else {
+        hitCount = 0;
+        explotePlayer();
+    }
+    hitCount++;
+});
 
+function explotePlayer() {
     if (!playerElem.classList.contains("dead")) {
 
       playerElem.classList.add("dead");
       setTimeout(() => {
         playerElem.classList.remove("dead");
       }, 1000);
+      
     }
+}
 
-});
+function hitPlayer() {
+    if (!playerElem.classList.contains("hit")) {
 
+      playerElem.classList.add("hit");
+      setTimeout(() => {
+        playerElem.classList.remove("hit");
+      }, 300);
+
+    }
+}
 
 const playButton = document.querySelector('#play-btn');
 
